34a06bc95a292ee8968c2c2165995aff69be96a6
YutoKondo26 <yuutokondou26@gmail.com>
2024-08-11 19:14:59 +0900

    名前付きスロット

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+    <x-slot class="heder">ヘッダー1</x-slot>
 component-test1
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,6 @@
 <x-tests.app>
+    <x-slot class="heder">ヘッダー2</x-slot>
+
 component-test2
 </x-tests.app>
     
